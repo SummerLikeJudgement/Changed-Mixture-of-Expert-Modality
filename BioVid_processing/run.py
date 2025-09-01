1fed409446da218497ba0068d5192fb8564530c8
summer <summer35174@gmail.com>
2025-09-01 18:05:38 +0800

    model update

--- BioVid_processing/run.py
+++ BioVid_processing/run.py
@@ -3,15 +3,20 @@ import os
 from ECG.train import main as ecg_train
 from GSR.train import main as gsr_train
 
+npz_dirs = {
+    "ecg":"./processed/ecg",
+    "gsr":"./processed/gsr"
+}
+
 
 def model_run(model_name = "", mode = ""):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     if model_name == 'ecg' and mode == 'train':
-        npz_dir = "./processed/ecg"
+        npz_dir = npz_dirs['ecg']
         abs_npz_dir = os.path.abspath(os.path.join(base_dir, npz_dir))
         ecg_train(data_dir = abs_npz_dir)
     elif model_name == 'gsr' and mode == 'train':
-        npz_dir = "./processed/gsr"
+        npz_dir = npz_dirs['gsr']
         abs_npz_dir = os.path.abspath(os.path.join(base_dir, npz_dir))
         gsr_train(data_dir = abs_npz_dir)
     else:
--- BioVid_processing/train.py
+++ BioVid_processing/train.py
@@ -1,3 +1,3 @@
 from run import model_run
 
-model_run("gsr", "train")
+model_run(model_name="gsr", mode="train")
